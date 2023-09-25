f07be5823b7ee74f252c9e1cd156129d1c1880b3
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2023-09-25 14:26:30 -0300

    ultimos cambios para el deploy

--- src/main/java/com/proyecto/coompitas/CoompitasApplication.java
+++ src/main/java/com/proyecto/coompitas/CoompitasApplication.java
@@ -1,20 +1,13 @@
 package com.proyecto.coompitas;
 
-import com.mercadopago.MercadoPagoConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoompitasApplication implements CommandLineRunner {
+public class CoompitasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoompitasApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception{
-        MercadoPagoConfig.setAccessToken("TEST-6874223181035381-091606-e986a9cfbf3c55c4c1a924de61bf8cb9-269860643");
     }
 
 }
